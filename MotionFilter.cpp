5a251c9e029a883ceb8366f73bc8f3e472317d7d
WindyLibra <97620223+WindyLibra@users.noreply.github.com>
2024-02-28 12:09:42 +0400

    Update MotionFilter.cpp

--- MotionFilter.cpp
+++ MotionFilter.cpp
@@ -16,6 +16,7 @@ using namespace cv;
 
 int main() {
 
+    //inputs 
     string input = "C:/Users/aram_/Downloads/humanwalking.mp4";
     //string input = "C:/Users/aram_/Downloads/timelapse.mp4";
     //string input = "C:/Users/aram_/Downloads/sixseconds.mp4";
@@ -28,39 +29,33 @@ int main() {
         return 0;
     }
 
+    //Mats
     Mat oldframe, newframe, result, result2;
     Mat gray_res, thresh_res;
 
+    // Frame/time variables
     int count = 0;
     double duration = 0;
     int skipFrames = 2;
     int frameCount = 0;
+    int resize_val = 4;
 
-    Mat kernel1 = getStructuringElement(MORPH_RECT, Size(1, 1));
-    Mat kernel3 = getStructuringElement(MORPH_RECT, Size(3, 3)); // MORPH_RECT, MORPH_CROSS, MORPH_ELLIPSE
-    Mat kernel5 = getStructuringElement(MORPH_RECT, Size(5, 5)); 
-    Mat kernel11 = Mat::zeros(11, 11, CV_8UC1);
+    // Kernels, they can be MORPH_RECT, MORPH_CROSS, MORPH_ELLIPSE
+    Mat kernel1 = getStructuringElement(MORPH_CROSS, Size(1, 1));
+    Mat kernel3 = getStructuringElement(MORPH_CROSS, Size(3, 3)); 
+    Mat kernel5 = getStructuringElement(MORPH_CROSS, Size(5, 5)); 
 
-    // Set the border values to 1
-    for (int i = 0; i < 11; ++i) {
-        kernel11.at<uchar>(0, i) = 1; // Top row
-        kernel11.at<uchar>(11 - 1, i) = 1; // Bottom row
-        kernel11.at<uchar>(i, 0) = 1; // Leftmost column
-        kernel11.at<uchar>(i, 11 - 1) = 1; // Rightmost column
-    }
+    // for connectedComponentsWithStats()
+    vector<Rect> filtered_rects;
+    vector<vector<Point>> components;
+    Mat labels, stats, centroids;
 
     namedWindow("Result", 0);
     namedWindow("Original", 0);
     namedWindow("Thresh", 0);
 
     capture >> oldframe;
-    resize(oldframe, oldframe, cv::Size(oldframe.cols / 3, oldframe.rows / 3));
-
-    vector<Rect> filtered_rects;
-    vector<vector<Point>> components;
-    Mat labels, stats, centroids;
-    int max_rect_area_threshold = 850;
-
+    resize(oldframe, oldframe, cv::Size(oldframe.cols / resize_val, oldframe.rows / resize_val));
 
     while (true)
     {
@@ -75,9 +70,7 @@ int main() {
 
         auto then = chrono::system_clock::now();
 
-        filtered_rects.clear();
-
-        resize(newframe, newframe, cv::Size(newframe.cols / 3, newframe.rows / 3));
+        resize(newframe, newframe, cv::Size(newframe.cols / resize_val, newframe.rows / resize_val));
         absdiff(oldframe, newframe, result);
 
         cvtColor(result, gray_res, COLOR_BGR2GRAY);
@@ -85,29 +78,30 @@ int main() {
         erode(result, result, kernel1, Point(-1, -1), 1);
         dilate(result, result, kernel3, Point(-1, -1), 1);
 
-        //filter2D(result, result2, -1, kernel11);
-        adaptiveThreshold(gray_res, thresh_res, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 11, 5);
+        //ADAPTIVE_THRESH_MEAN_C
+        adaptiveThreshold(gray_res, thresh_res, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV, 9, 5.5);
         imshow("Thresh", thresh_res);
 
-        int connected_components = connectedComponentsWithStats(thresh_res, labels, stats, centroids, 4, CV_32S);
+        int connected_components = connectedComponentsWithStats(thresh_res, labels, stats, centroids, 8, CV_32S); //connectivity can be 4 or 8
         for (int i = 1; i < connected_components; i++) { // Skip background (label 0)
             int area = stats.at<int>(i, CC_STAT_AREA);
-            if (area > 200 && area < max_rect_area_threshold) { // Adjust threshold based on image complexity
+            if (area > 100 && area < 750) { // Adjust threshold based on image complexity
                 Rect rect(stats.at<int>(i, CC_STAT_LEFT),
-                    stats.at<int>(i, CC_STAT_TOP),
-                    stats.at<int>(i, CC_STAT_WIDTH),
-                    stats.at<int>(i, CC_STAT_HEIGHT));
+                          stats.at<int>(i, CC_STAT_TOP),
+                          stats.at<int>(i, CC_STAT_WIDTH),
+                          stats.at<int>(i, CC_STAT_HEIGHT));
                 filtered_rects.push_back(rect);
             }
         }
 
         // Draw rectangles
         for (const auto& rect : filtered_rects) {
-            rectangle(result, rect.tl(), rect.br(), Scalar(0, 255, 0), 2);
+            rectangle(result, rect.tl(), rect.br(), Scalar(0, 255, 0), 1);
         }
 
-        imshow("Result", result * 2);
+        imshow("Result", result * 1.5);
         oldframe = newframe.clone();
+        filtered_rects.clear();
 
         auto now = chrono::system_clock::now();
 
@@ -117,7 +111,7 @@ int main() {
         count++;
         duration += elapsed_seconds.count();
 
-        int keyboard = waitKey(0);
+        int keyboard = waitKey(2);
         if (keyboard == 'q' || keyboard == 27)
             break;
 
